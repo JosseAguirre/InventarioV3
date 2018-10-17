2cb59508b5bdbcd26049891d0fdbbd12469d5977
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2018-10-17 12:18:45 -0500

    Cambios en las pantallas y tablas de la bd

---
+++ ModuloInventario/ModuloInventario/Controllers/ArticulosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ModuloInventario.Models;
+
+namespace ModuloInventario.Controllers
+{
+    public class ArticulosController : Controller
+    {
+        private InventarioContext db = new InventarioContext();
+
+        // GET: Articulos
+        public ActionResult Index()
+        {
+            return View(db.ARTICULO.ToList());
+        }
+
+        // GET: Articulos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ARTICULO aRTICULO = db.ARTICULO.Find(id);
+            if (aRTICULO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aRTICULO);
+        }
+
+        // GET: Articulos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SECUENCIAL,ARTICULO1,CATEGORIAARTICULO")] ARTICULO aRTICULO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ARTICULO.Add(aRTICULO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aRTICULO);
+        }
+
+        // GET: Articulos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ARTICULO aRTICULO = db.ARTICULO.Find(id);
+            if (aRTICULO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aRTICULO);
+        }
+
+        // POST: Articulos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SECUENCIAL,ARTICULO1,CATEGORIAARTICULO")] ARTICULO aRTICULO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aRTICULO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aRTICULO);
+        }
+
+        // GET: Articulos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ARTICULO aRTICULO = db.ARTICULO.Find(id);
+            if (aRTICULO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aRTICULO);
+        }
+
+        // POST: Articulos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ARTICULO aRTICULO = db.ARTICULO.Find(id);
+            db.ARTICULO.Remove(aRTICULO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ModuloInventario/ModuloInventario/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModuloInventario.MvcApplication" Language="C#" %>
